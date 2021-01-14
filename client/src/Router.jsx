892717f4006b5aa3e08f0b722993bfbab9d10138
yamaguchiosamu1997 <shankoudao1222@gmail.com>
2021-01-14 11:34:37 +0900

    createbuylist

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Switch } from "react-router"
 import { Calendar } from "./templates"
+import { BuyList } from "./templates"
 
 
 const Router = () => {
@@ -8,6 +9,7 @@ const Router = () => {
     
       <Switch>
         <Route exact path={"(/)?"} component={Calendar} />
+        <Route exact path={"/buylist"} component={BuyList} />
       </Switch>
     
     
